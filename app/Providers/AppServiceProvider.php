f71af560e48b648570c36b1e4a8214e987240929
manuelrecalde <manuelrecalde230497@gmail.com>
2026-08-25 22:15:03 -0300

    Migrar automatico en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::policy(Patient::class, PatientPolicy::class);
+      if ($this->app->environment('production')) {
+        Artisan::call('migrate', ['--force' => true]);
+    }
     }
 }
