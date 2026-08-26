30591eb80c7f680d5f9a301792c69659bf4f990d
manuelrecalde <manuelrecalde230497@gmail.com>
2026-08-26 10:49:13 -0300

    Importar Schema y Artisan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Providers;
+
 use App\Models\Patient;
 use App\Policies\PatientPolicy;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Si la tabla sessions no existe, ejecuta las migraciones automáticamente
+        if (app()->environment('production') && !Schema::hasTable('sessions')) {
+            Artisan::call('migrate', ['--force' => true]);
+        }
     }
 }
