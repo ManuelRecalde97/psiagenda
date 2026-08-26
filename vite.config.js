7894cf13eee1909aa2a0df41661c947651fb0951
manuelrecalde <manuelrecalde230497@gmail.com>
2026-08-26 12:19:24 -0300

    Corregir base URL de Vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: 'https://psiagenda-2il6.onrender.com/', // Agrega esta línea
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
