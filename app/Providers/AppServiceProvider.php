868e119363d4115f620b16264f98afb5b688fe89
manuelrecalde <manuelrecalde230497@gmail.com>
2026-08-25 22:17:16 -0300

    Limpiar AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,5 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-      if ($this->app->environment('production')) {
-        Artisan::call('migrate', ['--force' => true]);
-    }
-    }
+    
 }
